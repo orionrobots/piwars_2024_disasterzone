4b5acd296765a9fefea4763ef85db53dd953b141
Emma Seymour <emma_seymour@icloud.com>
2023-10-08 15:48:30 +0100

    method to test robot

---
+++ src/test_robot.py
@@ -0,0 +1,14 @@
+from adafruit_crickit_robot import Robot
+import time
+
+robot = Robot()
+
+robot.forwards()
+time.sleep(3)
+robot.backwards()
+time.sleep(3)
+robot.right()
+time.sleep(3)
+robot.left()
+time.sleep(3)
+robot.stop()
