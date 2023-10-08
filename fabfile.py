9f0c982d7b9fd2e5ec3b91b298baead0c4aa4b8e
Emma Seymour <emma_seymour@icloud.com>
2023-10-08 15:16:09 +0100

    additional setup

--- fabfile.py
+++ fabfile.py
@@ -2,12 +2,23 @@ from fabric import task
 
 @task
 def deploy_system(c):
-    c.run("sudo apt-get install -y python3-pip python3-smbus i2c-tools git")
-    c.run("sudo raspi-config nonint do_i2c 0")
-    c.run("sudo raspi-config nonint do_i2c 1")
-    c.run("sudo reboot")
+    c.sudo("apt-get install -y python3-pip python3-smbus i2c-tools git")
+    c.sudo("raspi-config nonint do_i2c 0") # 0 enables interface
+    c.sudo("apt-get update")
+    c.sudo("apt-get upgrade -y")
+    c.sudo("reboot")
 
 @task
 def deploy_adafruit_stepper_motor_hat(c):
-    c.run("sudo pip3 install Adafruit-Blinka")
-    c.run("sudo pip3 install adafruit-circuitpython-motorkit")
+    # maybe need RPI.GPIO
+    c.sudo("pip3 install Adafruit-Blinka")
+    c.sudo("pip3 install adafruit-circuitpython-motorkit")
+
+@task
+def show_i2c_devices(c):
+    c.run("/usr/sbin/i2cdetect -y 1")
+    c.run("/usr/sbin/i2cdetect -y 0")
+
+@task
+def power_off(c):
+    c.sudo("poweroff")
