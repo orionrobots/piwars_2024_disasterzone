0822a8a4d48fed5d9f2e264982fb44f6f0467022
Danny Staple <danny@orionrobots.co.uk>
2023-11-19 17:52:45 +0000

    Move the motors service to a demo service

---
+++ robot/services/demo/service.py
@@ -0,0 +1,7 @@
+# import ssl
+from robot.services.common.base_service import BaseService
+
+service = BaseService("demo")
+service.run()
+service.client.loop_forever()
+# could be loop(1) with some work
