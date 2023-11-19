8d1e7d523fde2ac83da88afa35ec5f87f2afc7bc
Danny Staple <danny@orionrobots.co.uk>
2023-11-19 12:48:17 +0000

    Add a settings system

---
+++ src/robot_settings.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    pi_hostname: str = "your_robot"
+    pi_username: str = "your_username"
+    mqtt_host: str = "localhost"
+    mqtt_port: int = 1883
+    mqtt_username: str = "your_mqtt"
+    mqtt_password: str = "your_mqtt_pass"
+
+
+# TODO: Add motor controller choice here
