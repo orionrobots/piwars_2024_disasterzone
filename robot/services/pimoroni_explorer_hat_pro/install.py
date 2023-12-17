82063e71318a54d5c9aa96861390d7d6e72e6daa
Emma Seymour <emma_seymour@icloud.com>
2023-12-17 15:09:16 +0000

    create installer for service

--- robot/services/pimoroni_explorer_hat_pro/install.py
+++ robot/services/pimoroni_explorer_hat_pro/install.py
@@ -1,10 +1,28 @@
-from pyinfra.operations import pip, systemd
+from pyinfra.operations import pip, systemd, files
+import os
 
 pip.packages(name="install packages", packages=["smbus2", "explorerhat"], present=True, _sudo=True)
 
+username = os.environ["PI_USERNAME"]
+
+# Create the service unit file
+files.template(
+    name="Create explorerhat service",
+    src="robot/services/pimoroni_explorer_hat/service.j2",
+    dest="/etc/systemd/system/explorerhat.service",
+    mode="644",
+    user="root",
+    group="root",
+    pi_username=username,
+    _sudo=True
+)
+
+# Enable the service
 systemd.service(
-    name="ExplorerHat",
+    name="Enable explorerhat service",
+    service="explorerhat.service",
     present=True,
     enabled=True,
     restarted=True,
-    command="python3 ")
+    _sudo=True
+)
