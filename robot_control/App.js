969cd47546a97bcb82d66bf27e0286db18f0b847
Emma Seymour <emma_seymour@icloud.com>
2023-12-17 16:04:49 +0000

    joystick

--- robot_control/App.js
+++ robot_control/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { KorolJoystick } from "korol-joystick";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <Text>Control the robot!</Text>
+      <KorolJoystick color="#06b6d4" radius={75} onMove={(data) => console.log(data)}></KorolJoystick>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
