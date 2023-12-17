3cb8231c9bb2a5db3ff40a618195f82c8d22cbdd
Danny Staple <danny@orionrobots.co.uk>
2023-12-17 13:35:09 +0000

    React app with joystick

--- web/robot_control/App.js
+++ web/robot_control/App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { KorolJoystick } from "korol-joystick";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <GestureHandlerRootView style={styles.container}>
+      <Text>Control the robot in GestureHandlerRootView </Text>
+      <KorolJoystick color="#06b6d4" radius={75} onMove={(data) => console.log(data)}></KorolJoystick>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
