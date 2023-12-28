14e8682d39294a15d86af9d7bb2ce4c657ebf41a
Danny Staple <danny@orionrobots.co.uk>
2023-12-28 11:33:09 +0000

    Some workign mqtt joystick

--- robot_control/App.js
+++ robot_control/App.js
@@ -3,15 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { KorolJoystick } from "korol-joystick";
 
+import Paho from "paho-mqtt"
+
 export default function App() {
   return (
     <GestureHandlerRootView style={styles.container}>
       <Text>Control the robot!</Text>
-      <KorolJoystick color="#06b6d4" radius={75} onMove={onJoystickMove} onStop={onJoyStickStop}></KorolJoystick>
+      <KorolJoystick color="#06b6d4" radius={75} onMove={onJoystickMove} onStop={onJoyStickStop} />
       <StatusBar style="auto" />
     </GestureHandlerRootView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -22,12 +25,68 @@ const styles = StyleSheet.create({
   },
 });
 
+var connected = false;
+// How to fetch the settings
+// https://stackoverflow.com/questions/65903774/receive-mqtt-messages-on-react-native-expo
+const mqttClient = new Paho.Client(
+  'orionrob-disaster.local',
+  Number(9001),
+  '/mqtt',
+  'robot_control'
+);
+
+const reconnect = function(and_do=() => {}) {
+  console.log('Connecting to MQTT broker');
+  mqttClient.connect({
+    userName: 'danny',
+    password: 'learnrob3_mqtt',
+    keepAliveInterval: 60,
+    reconnect: true,
+    onSuccess: () => {
+      console.log('Connected to MQTT broker');
+      connected = true;
+      and_do();
+    },
+    onFailure: (err) => {
+      console.log('Connection to MQTT broker failed');
+      console.log(err);
+      connected = false;
+    },
+  });
+}
+
+reconnect();
+
+mqttClient.onConnectionLost = (err) => {
+  console.log('Connection to MQTT broker lost');
+  console.log(err);
+  connected = false;
+};
+
 // Handlers for joystick
-const onJoystickMove = (data) => {
-  console.log('Joystick move');
-  console.log(data);
+const checkConnectionAndSend = (topic, payload) => {
+  data = JSON.stringify(payload);
+  if (!connected) {
+    reconnect(
+      () => {
+        mqttClient.publish(topic, data);
+      }
+    );
+  } else {
+    mqttClient.publish(topic, data);
+  }
+}
+
+const onJoystickMove = async (data) => {
+  // console.log('Joystick move');
+  // console.log(data);
+  let speed = Math.sin(data.angle.radian) * min(1, data.force);
+  let curve = Math.cos(data.angle.radian) * min(1, data.force);
+  console.log("speed: " + speed + "\t curve: " + curve);
+  checkConnectionAndSend('motors/forward', {speed: speed, curve: curve});
 }
 
 const onJoyStickStop = () => {
   console.log('Joystick stop');
+  checkConnectionAndSend('motors/stop', {});
 }
