49acbdb647c6dc26a07522b595ce78468aebcfbb
Danny Staple <danny@orionrobots.co.uk>
2023-12-31 18:29:33 +0000

    That file shouldn't have gone in.

--- robot_control/env.js
+++
@@ -1,6 +0,0 @@
-export const PI_HOSTNAME="192.168.0.206" //"piwars2024de.local"
-export const PI_USERNAME="e11110"
-// NOTE: No password, we use key access
-export const MQTT_USERNAME="robot"
-export const MQTT_PASSWORD="robot"
-export const BOARD_NAME="pimoroni_explorer_hat_pro"
