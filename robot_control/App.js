9bbbd3d30fa7a9a4b72215c52393f3477c877ad9
Fun-Fun.design <76519320+PiPiHand@users.noreply.github.com>
2023-12-31 16:08:45 +0000

    joystick control that doesnt work :|

--- robot_control/App.js
+++ robot_control/App.js
@@ -1,7 +1,11 @@
+import Paho from "paho-mqtt";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { KorolJoystick } from "korol-joystick";
+import { styles } from "./styles";
+import * as env from "./env";
+
 
 export default function App() {
   return (
@@ -13,21 +17,43 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const mqttClient = new Paho.Client(
+  env.PI_HOSTNAME,
+  Number(9001),
+  "./mqtt",
+  "robot_control"
+);
+
+mqttClient.onConnectionLost = (err) => {
+  console.log("Connected to MQTT broker lost");
+  console.log(err);
+};
+
+console.log("Attempting to connect to " + env.PI_HOSTNAME);
+mqttClient.connect({
+  userName: env.MQTT_USERNAME,
+  password: env.MQTT_PASSWORD,
+  onSuccess: () => {
+    console.log("Connected");
+  },
+  onFailure: (err) => {
+    console.log('Connection failed');
+    console.log(err)
   },
 });
 
 // Handlers for joystick
 const onJoystickMove = (data) => {
-  console.log('Joystick move');
-  console.log(data);
-}
+  // console.log('Joystick move');
+  // console.log(data);
+  let speed = Math.sin(data.angle.radian) * Math.min(1, data.force);
+  let curve = Math.cos(data.angle.radian) * Math.min(1, data.force);
+  console.log("speed: "+speed+"\t curve: "+curve);
+  data = JSON.stringify({speed: speed, curve: curve});
+  mqttClient.publish("motors/forward", data);
+};
 
 const onJoyStickStop = () => {
-  console.log('Joystick stop');
-}
+  // console.log('Joystick stop');
+  mqttClient.publish("motors/stop", "{}");
+};
