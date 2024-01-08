29660b34f6cd6553774e3b63bea33de9815e9fe0
Danny Staple <danny@orionrobots.co.uk>
2024-01-08 18:20:54 +0000

    Make use of common  service parts

--- robot/services/pimoroni_inventor_hat_mini/install.py
+++ robot/services/pimoroni_inventor_hat_mini/install.py
@@ -1,29 +1,20 @@
-from pyinfra.operations import pip, systemd, files
+from pyinfra.operations import systemd, files
 from deploy.helpers import system_pip
 from robot.common.settings import RobotSettings
+from deploy import deploy_common_environment
 settings = RobotSettings()
 
 results = [
-    system_pip(name="install packages", packages=["smbus2", "inventorhatmini", "pydantic-settings"], present=True, _sudo=True),
-
-    # Update the environment
-    files.put(
-        name="Copy the environment file",
-        src=".env",
-        dest="/etc/robot.env",
-        _sudo=True
-    ),
+    deploy_common_environment.common_packages,
+    deploy_common_environment.environment_file,
+    deploy_common_environment.common_folder,
+    system_pip(name="install packages", packages=["smbus2", "inventorhatmini"], present=True, _sudo=True),
+    
     # Update the service source code
     files.put(
         name="Copy the service source code",
         src="robot/services/pimoroni_inventor_hat_mini/service.py",
         dest="robot/services/pimoroni_inventor_hat_mini/service.py",
-    ),
-    # Update the common folder
-    files.sync(
-        name="Copy the common folder",
-        src="robot/common",
-        dest="robot/common"
     ),
     # Create the service unit file
     files.template(
