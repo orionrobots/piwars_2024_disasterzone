36cf4990ebe75f38a3e423dd7a368848869e5571
Danny Staple <danny@orionrobots.co.uk>
2024-01-10 21:35:31 +0000

    We have Mpremote commends. Could do with refactor, but it works!

---
+++ robot/services/pimoroni_yukon/install.py
@@ -0,0 +1,62 @@
+from typing import List
+import os
+
+from pyinfra.operations import files, pip
+from pyinfra.api import operation, StringCommand, FileUploadCommand, FactBase
+from pyinfra import host, local
+
+MPREMOTE_LOCATION=f"/home/{host.data.get('ssh_user')}/.local/bin/mpremote"
+
+class MpRemoteFiles(FactBase):
+    def command(self, path):
+        return f"{MPREMOTE_LOCATION} ls {path}"
+
+    def process(self, output: List[str]):
+        full_list = (line.strip() for line in output)
+        size_name_list = [line.split(" ") for line in full_list]
+        filtered_list = [(int(item[0]), item[1]) for item in size_name_list if item[0] != 'ls']
+
+        return filtered_list
+
+
+@operation
+def mpremote_sync_file(src, dest):
+    """Sync source files to a micropython device
+    with mpremote.
+    src=local folder
+    dest=folder on micropython device attached to the remote robot
+    """
+    pi_dest = f"mpremote/{dest}"
+    if '/' in dest:
+        mpremote_folder, file_name = dest.rsplit('/', 1)
+    else:
+        mpremote_folder = "/"
+        file_name = dest
+    
+    folder_content = host.get_fact(MpRemoteFiles, path=mpremote_folder)
+    # If the file exists
+    if file_name in [item[1] for item in folder_content]:
+        # and has the same size
+        remote_file_size = [item for item in folder_content if item[1] == file_name][0][0]
+        local_file_size = os.stat(src).st_size
+        if remote_file_size == local_file_size:
+            # Check the content of the remote file and compare
+            return
+    
+    # update the robot copy
+    yield from files.put( src, pi_dest)
+    mpremote_command = f"{MPREMOTE_LOCATION} cp {pi_dest} :{dest}"
+    print(mpremote_command)
+    yield StringCommand(mpremote_command)
+
+@operation
+def mpremote_reset():
+    """Reset the micropython device
+    with mpremote.
+    """
+    yield StringCommand(f"{MPREMOTE_LOCATION} reset")
+
+pip.packages(name="Install mpremote", packages=["mpremote"])
+file_sync = mpremote_sync_file(name="Copy test file", src="robot/services/pimoroni_yukon/rp2040/test_flash_leds.py", dest="main.py")
+if file_sync.changed:
+    mpremote_reset(name="Reset the Yukon")
--- robot/services/pimoroni_yukon/rp2040/test_flash_leds.py
+++ robot/services/pimoroni_yukon/rp2040/test_flash_leds.py
@@ -1,3 +1,28 @@
 """Test code for Yukon board.
 Flash the LED's in sequence.
+Derivative of https://github.com/pimoroni/yukon/blob/main/examples/board/blink_led.py
+Press "Boot/User" to exit the program.
 """
+from pimoroni_yukon import Yukon
+
+# Constants
+SLEEP = 0.25    # The time to sleep between each toggle
+
+# Variables
+yukon = Yukon()     # A new Yukon object
+led_state = False
+
+# Wrap the code in a try block, to catch any exceptions (including KeyboardInterrupt)
+try:
+    # Loop until the BOOT/USER button is pressed
+    while not yukon.is_boot_pressed():
+
+        led_state = not led_state           # Toggle the LED state
+        yukon.set_led('A', led_state)       # Set the LED to the new state
+        yukon.set_led('B', not led_state)   # Set the other LED to the opposite state
+
+        yukon.monitored_sleep(SLEEP)                   # Sleep for a number of seconds
+        yukon.print_readings()
+finally:
+    # Put the board back into a safe state, regardless of how the program may have ended
+    yukon.reset()
