6391b023adc64504a7eda36ddccb44f613e7dc7f
Danny Staple <danny@orionrobots.co.uk>
2024-01-21 15:19:50 +0000

    We need to stop the service before we can send files to the Yukon.

--- robot/services/pimoroni_yukon/install.py
+++ robot/services/pimoroni_yukon/install.py
@@ -1,17 +1,16 @@
 
 
 
-from pyinfra.operations import pip, apt
+from pyinfra.operations import pip, apt, systemd
 
 from deploy.helpers.mpremote_tools import mpremote_sync_file, mpremote_reset
 from deploy.helpers.python_service import deploy_python_service
 
 pip.packages(name="Install mpremote", packages=["mpremote"])
-# file_sync = mpremote_sync_file(name="Copy test file", src="robot/services/pimoroni_yukon/rp2040/test_flash_leds.py", dest="main.py")
-# if file_sync.changed:
-#     mpremote_reset(name="Reset the Yukon")
-
 apt.packages(name="Install packages", packages=['python3-serial'], present=True, _sudo=True)
+
+# Stop the service to release the serial port
+systemd.service(service="yukon", running=False)
 
 file_sync = mpremote_sync_file(name="Copy main", src="robot/services/pimoroni_yukon/rp2040/main.py", dest="main.py")
 if file_sync.changed:
@@ -22,5 +21,6 @@ deploy_python_service(
     service_module="robot.services.pimoroni_yukon.service",
     service_name="yukon",
     service_description="Yukon board service",
+    must_restart=True, # restart, as we stop the service to release the serial port
     _sudo=True
 )
