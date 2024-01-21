306b9d2a85c585f9343c8dcd7219603c43f0e984
Danny Staple <danny@orionrobots.co.uk>
2024-01-21 12:37:47 +0000

    Re-enable system pip

--- deploy/helpers/pip_helper.py
+++ deploy/helpers/pip_helper.py
@@ -6,5 +6,5 @@ from pyinfra import host
 
 print(host.get_fact(server.LsbRelease))
 
-# system_pip = functools.partial(pip.packages, 
-#                                extra_install_args="--root-user-action ignore --break-system-packages")
+system_pip = functools.partial(pip.packages, 
+                               extra_install_args="--root-user-action ignore --break-system-packages")
