6fc96c950dd9f7ab69b88d12e6cb0850b8c8cc10
Danny Staple <danny@orionrobots.co.uk>
2024-01-21 12:50:22 +0000

    Update common has workig pip.

--- deploy/update_common.py
+++ deploy/update_common.py
@@ -1,4 +1,8 @@
 from pyinfra.operations import files
+from deploy.helpers.pip_helper import system_pip
+
+common_pip_packages = system_pip(
+    name="install common packages", packages=["pydantic-settings"], present=True, _sudo=True),
 
 common_files = files.sync("robot/common", "robot/common")
 environment = files.put(
@@ -8,4 +12,4 @@ environment = files.put(
         _sudo=True
     )
 
-common_changed = common_files.changed or environment.changed
+common_changed = common_files.changed or environment.changed or common_pip_packages.changed
