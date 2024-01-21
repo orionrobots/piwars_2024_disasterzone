4951a845ac69bab064e3a3ef2bcba20916687e3e
Danny Staple <danny@orionrobots.co.uk>
2024-01-21 12:27:11 +0000

    Share the test pip helper

--- deploy/helpers/pip_helper.py
+++ deploy/helpers/pip_helper.py
@@ -1,5 +1,10 @@
 import functools
 
 from pyinfra.operations import pip
+from pyinfra.facts import server
+from pyinfra import host
 
-system_pip = functools.partial(pip.packages, extra_install_args="--root-user-action ignore --break-system-packages")
+print(host.get_fact(server.LsbRelease))
+
+# system_pip = functools.partial(pip.packages, 
+#                                extra_install_args="--root-user-action ignore --break-system-packages")
